5b760f1458793e041c1b48eb3828957235404782
Ossama <oussama.elk.207@gmail.com>
2023-03-19 18:30:49 -0700

    task done +++

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main : entry point.
+ *
+ * Description : a program to print text.
+ *
+ * return : 0 means success .
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
